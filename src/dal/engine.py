986ca7d764c7d9b5135fdb61bfa2ddb96d173d51
Tal <zadok.tal1@gmail.com>
2021-09-25 14:53:13 +0300

    added docker support

--- src/dal/engine.py
+++ src/dal/engine.py
@@ -1,7 +1,7 @@
 import databases
 import sqlalchemy
 
-DATABASE_URL = 'mysql://nfc:Password1@192.168.50.131:49164/nfc'
+DATABASE_URL = 'mysql://nfc:Password1@192.168.50.131:3306/nfc'
 
 engine = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
