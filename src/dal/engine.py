b6e4fc6533cb135bc0c6bf67ecc16e69b25b056b
Tal Tzadok <zadok42@protonmail.com>
2022-03-20 21:15:00 +0200

    fixed envrion

--- src/dal/engine.py
+++ src/dal/engine.py
@@ -3,7 +3,7 @@ import os
 import databases
 import sqlalchemy
 
-DATABASE_URL = os.environ('DATABASE_URL', 'sqlite:///:memory:')
+DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///:memory:')
 
 engine = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
