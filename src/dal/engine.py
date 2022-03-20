0ebd22c78f69429933deb3935274097d1b102b29
Tal Tzadok <zadok42@protonmail.com>
2022-03-20 21:07:20 +0200

    changed database url

--- src/dal/engine.py
+++ src/dal/engine.py
@@ -1,7 +1,7 @@
 import databases
 import sqlalchemy
 
-DATABASE_URL = 'postgres://xweiyffiuiqmvd:bd1e12823896aa023ceef4fb9a9dcb46a6af15a9ebb6eb87ed56b50fdb7e0f12@ec2-34-247-151-118.eu-west-1.compute.amazonaws.com:5432/d6r40rircnqa7b'
+DATABASE_URL = 'postgres://agewjmtkdkoftx:811470451404496aa962851b970f1943f972b31b7f8a168695eba0ffde7b2ec8@ec2-54-155-5-151.eu-west-1.compute.amazonaws.com:5432/d4me37f0v7drvr'
 
 engine = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
